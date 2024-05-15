cba4c7d77bac41bf8a2a1a52560ddb6b984796a7
planck <987379867@qq.com>
2024-05-15 20:12:11 +0800

    2024.5.15

---
+++ 2589.完成所有任务的最少时间.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=2589 lang=java
+ *
+ * [2589] 完成所有任务的最少时间
+ */
+
+// @lc code=start
+class Solution {
+    public int findMinimumTime(int[][] tasks) {
+        // int[] time=new int[2000];
+        Arrays.sort(tasks,(a,b)->{
+            return a[1]-b[1];
+        });
+        int ans=0;
+        
+    }
+}
+// @lc code=end
+
