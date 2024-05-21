d6ef3624c3f4280c8b45fdd795ab31c4ddd705ba
planck <987379867@qq.com>
2024-05-21 20:13:59 +0800

    2024.5.21

---
+++ 2769.找出最大的可达成数字.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=2769 lang=java
+ *
+ * [2769] 找出最大的可达成数字
+ */
+
+// @lc code=start
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+}
+// @lc code=end
+
