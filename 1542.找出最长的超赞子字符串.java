678c12b4fd5ab8b49b9dee26cd350eee2338d762
planck <987379867@qq.com>
2024-05-21 19:51:30 +0800

    2024.5.20

---
+++ 1542.找出最长的超赞子字符串.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1542 lang=java
+ *
+ * [1542] 找出最长的超赞子字符串
+ */
+
+// @lc code=start
+class Solution {
+    public int longestAwesome(String s) {
+
+    }
+}
+// @lc code=end
+
