f003bd3f2a6384bf7b2a7af30519b6fbcc4b5e30
planck <987379867@qq.com>
2024-05-28 23:22:10 +0800

    2024.5.28

---
+++ 2951.找出峰值.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=2951 lang=java
+ *
+ * [2951] 找出峰值
+ */
+
+// @lc code=start
+class Solution {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> ans=new ArrayList<>();
+        for(int i=1;i<mountain.length-1;i++){
+            if(mountain[i]>mountain[i-1]&&mountain[i]>mountain[i+1])
+            {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
